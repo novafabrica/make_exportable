73b793b1f6b0c7f0c44e10efb5273127b1e7041c
Matthew Bergman <mzbphoto@gmail.com>
2010-05-28 19:50:09 -0400

    fixed tests on both versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,14 @@ $LOAD_PATH << "." unless $LOAD_PATH.include?(".")
 begin
   require "rubygems"
   require "bundler"
+  require "active_record"
+  require "fastercsv" unless ActiveRecord::VERSION::MAJOR >= 3
+  
+  
 
   if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
     raise RuntimeError, "Your bundler version is too old." +
-     "Run `gem install bundler` to upgrade."
+      "Run `gem install bundler` to upgrade."
   end
 
   # Set up load paths for all bundled gems
@@ -17,6 +21,7 @@ rescue Bundler::GemNotFound
 end
 
 Bundler.require
+
 require File.expand_path('../../lib/make_exportable', __FILE__)
 
 
@@ -25,17 +30,17 @@ ENV['DB'] ||= 'sqlite3'
 database_yml = File.expand_path('../database.yml', __FILE__)
 if File.exists?(database_yml)
   active_record_configuration = YAML.load_file(database_yml)[ENV['DB']]
-  
+
   ActiveRecord::Base.establish_connection(active_record_configuration)
   ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-  
+
   ActiveRecord::Base.silence do
     ActiveRecord::Migration.verbose = false
-    
+
     load(File.dirname(__FILE__) + '/schema.rb')
     load(File.dirname(__FILE__) + '/models.rb')
-  end  
-  
+  end
+
 else
   raise "Please create #{database_yml} first to configure your database. Take a look at: #{database_yml}.sample"
 end
@@ -46,5 +51,3 @@ def clean_database!
     ActiveRecord::Base.connection.execute "DELETE FROM #{model.table_name}"
   end
 end
-
-clean_database!
