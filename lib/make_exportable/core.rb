cd4a456525692ee40a43cba3d69d73105d083444
Kevin Skoglund <kevin@novafabrica.com>
2010-06-16 17:38:31 -0400

    Split up data finding and mapping; improved handling of headers

--- lib/make_exportable/core.rb
+++ lib/make_exportable/core.rb
@@ -61,6 +61,7 @@ module MakeExportable #:nodoc:
       options.slice!(*valid_options)
 
       # Determine the exportable formats, default to all registered formats
+      # TODO: Add support for :formats option; maybe options[:as] ||= options[:formats]
       options[:formats] = MakeExportable.exportable_formats.keys
       if format_options = options.delete(:as)
         options[:formats] = options[:formats] & Array.wrap(format_options).map(&:to_sym)
@@ -73,18 +74,16 @@ module MakeExportable #:nodoc:
 
       # Determine the exportable columns, default to all columns and then
       # remove columns using the :only and :except options
-      options[:columns] = self.column_names.map(&:to_sym)
+      options[:columns] = column_names.map(&:to_sym)
       if only_options = options.delete(:only)
-        options[:columns] = Array.wrap(only_options).map {|i| i.to_sym}
+        options[:columns] = Array.wrap(only_options).map(&:to_sym)
+      elsif except_options = options.delete(:except)
+        options[:columns] = options[:columns] - Array.wrap(except_options).map(&:to_sym)
       end
-      if except_options = options.delete(:except)
-        options[:columns] = options[:columns] - Array.wrap(except_options).map {|i| i.to_sym}
-      end
-
 
       options[:scopes] ||= []
 
-      # exportable options will be :formats, :columns, :scopes
+      # exportable options will be :formats, :columns, :scopes & find options
       write_inheritable_attribute :exportable_options, options
       class_inheritable_reader :exportable_options
 
@@ -113,7 +112,7 @@ module MakeExportable #:nodoc:
       return true
     end
 
-    # <tt>to_export</tt> is a class method to export all records of a class. It can be called 
+    # <tt>to_export</tt> exports records from a class. It can be called 
     # directly on an ActiveRecord class, but it can also be called on an ActiveRelation scope.
     # It takes two arguments: a format (required) and a hash of options (optional).
     #
@@ -128,96 +127,63 @@ module MakeExportable #:nodoc:
     #   
     # Examples:
     #
-    #   User.to_export(:xml, :columns => [:first_name, :last_name, :username], 
+    #   User.to_export(:xml, :only => [:first_name, :last_name, :username], 
     #      :order => 'users.last_name ASC')
     #
     #   User.visible.sorted_by_username.to_export('csv', 
     #      :only => [:first_name, :last_name, :username])
     #
-    # As a convenience, you can also use "dynamically-named methods" and include the format 
-    # in the method name instead of providing it the first argument.
-    #
-    # Example:  User.to_xml_export(:columns => [:first_name, :last_name, :username])
-    #
     def to_export(format, options={})
-      #Simple fix till detangle we are having options merge twice because of !
-      clone_options = options.clone
-      data_set = self.get_export_data(options)
-      return self.create_report(format, data_set, clone_options)
+      export_data = get_export_data(options)
+      # remove the auto-headers from the export_data (i.e. the first row)
+      auto_headers = export_data.shift
+      # Use auto-headers unless given alternates or false (use no headers)
+      options[:headers] = auto_headers unless !options[:headers].blank? || options[:headers] === false
+      export_string = create_report(format, export_data, :headers => options[:headers])
+      return export_string
     end
-
-    # <tt>get_export_data</tt> is a class method that finds all objects of a given
-    # class using the options passed in and outputs an ordered array of arrays
-    # containing data to be used as columns of data with create_report. Valid options 
-    # include :only, :except, :scopes, and standard find options. 
-    # See <tt>to_export</tt> for more details on the available options.
-    # 
-    # Example:
+    
+    # <tt>get_export_data</tt> finds records for export using a combination of the default 
+    # export options and the argument options, and returns an array of arrays representing 
+    # the rows and columns of the export data. The first item ("row") in the array will be 
+    # an array of strings to be used as column headers.
+    # Valid options include :only, :except, :scopes and the standard find options. 
+    # See <tt>to_export</tt> for more details on the options.
     #
-    #   User.get_export_data(:columns => [:first_name, :last_name, :username])
-    #   #> [['John', 'Doe', 'johndoe'], ['Joe', 'Smith', 'jsmith']]
+    # Example:
+    # 
+    #   User.get_export_data(:only => [:first_name, :last_name, :username])
+    #   # => [['first_name', 'last_name', 'username'], ['John', 'Doe', 'johndoe'], ['Joe', 'Smith', 'jsmith']] }
     #
     def get_export_data(options={})
-      # remove any invalid options
-      valid_options = [:only, :except, :scopes, :conditions, :order, :include,
-                       :group, :having, :limit, :offset, :joins]
-      options.slice!(*valid_options)
-      # merge with default options
-      options.reverse_merge!(exportable_options)
-
-      if only_options = options.delete(:only)
-        options[:columns] = Array.wrap(only_options).map {|i| i.to_sym}
-      end
-      if except_options = options.delete(:except)
-        options[:columns] = options[:columns] - Array.wrap(except_options).map {|i| i.to_sym}
-      end
-
-      # apply scopes and then find options
-      collection = self
-      options[:scopes].each do |scope|
-        collection = collection.send(scope)
-      end
-      # For Rails 2.3 compatibility
-      if ActiveRecord::VERSION::MAJOR < 3
-        find_options = options.slice(:conditions, :order, :include, :group, :having, :limit, :offset, :joins)
-        collection = collection.find(:all, find_options)
-      else
-        # they should not be sending find options anymore, so we don't support them
-        collection = collection.all
-      end
-
-      rows = collection.map do |item|
-        options[:columns].map {|col| item.export_attribute(col) }
-      end
-      return rows
+      column_options = options.slice(:only, :except)
+      records     = find_export_data(options)
+      export_data = map_export_data(records, column_options)
+      return export_data
     end
 
-    # <tt>create_report</tt> is a class method to create a report from a set of data.
-    # It takes three arguments: a format (required), the data set to use for the report (required),
-    # and a hash of options (optional).  The only meaningful options are :columns and :headers either of 
-    # which will be used as headers for the columns in the data_set.
-    def create_report(format, data_set=[], options={})
-      validate_export_format(format)
-
-      # remove any invalid options
-      options.slice!(:only, :except, :headers)
-      # merge with default options
-      options.reverse_merge!(exportable_options)      
-      if only_options = options.delete(:only)
-        options[:columns] = Array.wrap(only_options).map {|i| i.to_sym}
+    # <tt>create_report</tt> creates a report from a set of data. It takes three arguments: 
+    # a format, the data set to use for the report, and an optional hash of options. 
+    # The only meaningful option is :headers which sets the strings to be used as column 
+    # headers for the data set. The value of :headers can be:
+    # * true - headers are the first row in the data set
+    # * false - headers are not in the data set and should not be added
+    # * array of strings to use for the column headers
+    # 
+    # The length of the headers must match the length of each row in the data set.
+    def create_report(format, data_set, options={})
+      if options[:headers] === true
+        options[:headers] = data_set.shift
       end
-      if except_options = options.delete(:except)
-        options[:columns] = options[:columns] - Array.wrap(except_options).map {|i| i.to_sym}
-      end
-      options[:headers] ||= options[:columns].map(&:to_s)
-
-      validate_data_lengths(data_set, options[:headers])
       
+      validate_export_format(format)
+      validate_export_data_lengths(data_set, options[:headers])
+      
+      # TODO: Add support in all formats classes for :headers == false
       format_class = MakeExportable.exportable_formats[format.to_sym]
       formater = format_class.new(data_set, options[:headers])
       return formater.generate, formater.mime_type
     end
-
 
     private
 
@@ -236,6 +202,61 @@ module MakeExportable #:nodoc:
         end
       end
 
+      # <tt>find_export_data</tt> finds all objects of a given
+      # class using a combination of the default export options and the options passed in.
+      # Valid options include :scopes and the standard find options. It returns a collection of 
+      # objects matching the find criteria.
+      # See <tt>to_export</tt> for more details on the options.
+      def find_export_data(options={})
+        
+        # merge with defaults then pull out the supported find and scope options
+        merged_options = options.reverse_merge(exportable_options)
+        find_options  = merged_options.slice(:conditions, :order, :include, :group, :having, :limit, :offset, :joins)
+        scope_options = merged_options.slice(:scopes)
+
+        # apply scopes and then find options
+        collection = self
+        scope_options[:scopes].each do |scope|
+          collection = collection.send(scope)
+        end
+        # For Rails 2.3 compatibility
+        if ActiveRecord::VERSION::MAJOR < 3
+          collection = collection.find(:all, find_options)
+        else
+          # they should not be sending find options anymore, so we don't support them
+          collection = collection.all
+        end
+        return collection
+      end
+
+      # <tt>map_export_data</tt> takes a collection and outputs an array of arrays representing 
+      # the rows and columns of the export data. The first item ("row") in the array will be 
+      # an array of strings to be used as column headers.
+      # Valid options include :only and :except.
+      # See <tt>to_export</tt> for more details on the options.
+      # 
+      #   User.map_export_data(User.visible, :only => [:first_name, :last_name, :username])
+      #   # => [['first_name', 'last_name', 'username'], ['John', 'Doe', 'johndoe'], ...]
+      #
+      def map_export_data(collection, options={})
+        # Use :only and :except options or else use class defaults for columns.
+        if !options[:only].blank?
+          options[:columns] = Array.wrap(options[:only]).map(&:to_sym)
+        elsif !options[:except].blank?
+          options[:columns] = column_names.map(&:to_sym) - Array.wrap(options[:except]).map(&:to_sym)
+        else
+          options[:columns] = exportable_options[:columns]
+        end
+        # TODO: validate_columns_not_empty
+
+        # TODO: Go ahead and humanize/titleize the column names here
+        headers = options[:columns].map(&:to_s)
+        rows = collection.map do |item|
+          options[:columns].map {|col| item.export_attribute(col) }
+        end
+        return rows.unshift(headers)
+      end
+
       # <tt>validate_export_format</tt> ensures that the requested export format is valid.
       def validate_export_format(format)
         unless MakeExportable.exportable_formats.keys.include?(format.to_sym)
@@ -246,9 +267,9 @@ module MakeExportable #:nodoc:
         end
       end
 
-      # <tt>validate_data_lengths</tt> ensures that the headers and all data rows are of the 
+      # <tt>validate_export_data_lengths</tt> ensures that the headers and all data rows are of the 
       # same size. (This is an important data integrity check if you are using NoSQL.)
-      def validate_data_lengths(data_set, data_headers=nil)
+      def validate_export_data_lengths(data_set, data_headers=nil)
         row_length = !data_headers.blank? ? data_headers.size : data_set[0].size
         if data_set.any? {|row| row_length != row.size }
           raise MakeExportable::ExportFault.new("Headers and all rows in the data set must be the same size.")
